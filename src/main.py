6e973f63a00347dead53fc71bc5b24ab97c3ba09
Yan Prada <yanpradamoro@gmail.com>
2024-03-23 19:30:47 -0300

    :test_tube: test (pylint): add test to pylint

--- src/main.py
+++ src/main.py
@@ -1,2 +1,26 @@
+"""
+Modulo para testes
+"""
+
+import pandas as pd
+
+
 def funcao_main():
+    """funcao que retorna algo"""
     return 0
+
+
+def clone():
+    """
+    esta
+    """
+    df = pd.read_parquet("")
+    return df
+
+
+def run_server():
+    """
+    docstring
+    """
+    df = pd.read_parquet("")
+    return df
