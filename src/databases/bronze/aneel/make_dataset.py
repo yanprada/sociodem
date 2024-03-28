484f23856cf5d2c88decad14c62d00d0175f0f5c
Yan Prada <yanpradamoro@gmail.com>
2024-03-27 22:17:43 -0300

    :sparkles: feat (aneel): add aneel company id database preprocess script

--- src/databases/bronze/aneel/make_dataset.py
+++ src/databases/bronze/aneel/make_dataset.py
@@ -1,30 +1,56 @@
-# # -*- coding: utf-8 -*-
-# import click
-# import logging
-# from pathlib import Path
-# from dotenv import find_dotenv, load_dotenv
+"""
+This module provides functions to load ANEEL IDs from a CSV file, 
+split the tags column into multiple columns,
+and filter the DataFrame by the year 2023.
+"""
+
+import pandas as pd
+from src.tools.utils.config import get_contract
+from src.tools.utils.common import save_parquet_decorator
+
+CONTRACT = get_contract("contract_aneel.yaml")
+DATABASE = CONTRACT["bronze"][0]
+TABLE_NAME = DATABASE["tableName"]
+YEAR = DATABASE["queryYear"]
 
 
-# @click.command()
-# @click.argument("input_filepath", type=click.Path(exists=True))
-# @click.argument("output_filepath", type=click.Path())
-# def main(input_filepath, output_filepath):
-#     """Runs data processing scripts to turn raw data from (../raw) into
-#     cleaned data ready to be analyzed (saved in ../processed).
-#     """
-#     logger = logging.getLogger(__name__)
-#     logger.info("making final data set from raw data")
+def load_aneel_ids() -> pd.DataFrame:
+    """
+    Loads ANEEL IDs from a CSV file and returns them as a pandas DataFrame.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing ANEEL IDs.
+    """
+    columns = [col["column"] for col in DATABASE.columns]
+    df = pd.read_csv(DATABASE.path, usecols=columns)
+    return df
 
 
-# if __name__ == "__main__":
-#     log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-#     logging.basicConfig(level=logging.INFO, format=log_fmt)
+def split_tags(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Splits the tags column into multiple columns.
 
-#     # not used in this stub but often useful for finding various files
-#     project_dir = Path(__file__).resolve().parents[2]
+    Args:
+        df (pd.DataFrame): The DataFrame to split.
 
-#     # find .env automagically by walking up directories until it's found, then
-#     # load up the .env entries as environment variables
-#     load_dotenv(find_dotenv())
+    Returns:
+        pd.DataFrame: The DataFrame with the tags column split.
+    """
+    tags = (
+        df["tags"].str.split(",", expand=True).rename(columns={0: "company", 1: "year"})
+    )
+    df = pd.concat([df, tags], axis=1)
+    df = df.drop(columns=["tags", 2])
+    return df
 
-#     main()
+
+@save_parquet_decorator(medallon="silver", filename=TABLE_NAME)
+def main() -> pd.DataFrame:
+    """
+    This function loads ANEEL IDs, splits tags, and returns a DataFrame filtered by the year 2023.
+
+    Returns:
+        pandas.DataFrame: A DataFrame containing ANEEL Company IDs filtered by the year 2023.
+    """
+    df = load_aneel_ids().pipe(split_tags).query(f"year == '{YEAR}'")
+    return df
