26e1ed955856e3169e7e986cd6a270ca6be67b63
Yan Prada <yanpradamoro@gmail.com>
2024-03-27 22:18:46 -0300

    :sparkles: feat (tools): add function to get contract files

---
+++ src/tools/utils/config.py
@@ -0,0 +1,29 @@
+"""
+This module contains utility functions for working with configuration files.
+"""
+
+import os
+from functools import lru_cache
+from pathlib import Path
+import yaml
+from easydict import EasyDict
+
+
+@lru_cache(maxsize=1)
+def get_contract(contract: str = "contract_template.yaml") -> EasyDict:
+    """
+    Get the configuration file for contract databases.
+
+    Args:
+        contract (str): The name of the contract file to be loaded.
+                        Default is "contract_template.yaml".
+    Returns:
+        EasyDict: A dictionary-like object that allows attribute access to its keys.
+    """
+    file_path = os.path.abspath(__file__)
+    directory = os.path.dirname(file_path)
+    path_src = Path(directory).parent.parent.absolute()
+    config_file = os.path.join(path_src, "databases/data_contract/", contract)
+    with open(config_file, "r", encoding="utf-8") as file:
+        config = yaml.safe_load(file)
+    return EasyDict(config)
