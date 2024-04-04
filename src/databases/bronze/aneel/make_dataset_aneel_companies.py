0268999a21b06dead15611cf4afa0da1a3504227
Yan Prada <yanpradamoro@gmail.com>
2024-04-04 08:23:21 -0300

    :sparkles: feat (aneel companies): add functions to read ponnot and ucbt files

--- src/databases/bronze/aneel/make_dataset_aneel_companies.py
+++ src/databases/bronze/aneel/make_dataset_aneel_companies.py
@@ -13,22 +13,23 @@ Usage:
 
 """
 
+import gc
 import pandas as pd
+import geopandas as gpd
+from tqdm import tqdm
 from src.tools.utils.config import get_contract
 from src.tools.databases.connection import DBConnectionHandler
 from src.tools.databases.data_request.drivers.http_requester import HttpRequesterAneel
-
-# from src.tools.utils.save import save_parquet_decorator
+from src.tools.utils.save import save_parquet_decorator
 from src.tools.utils.read import Reader
 
 
 MEDALLON_INPUT_CONTRACT = "silver"
 CONTRACT_INPUT = get_contract("contract_aneel_companies_id.yaml")
 DATABASE_INPUT_CONTRACT = CONTRACT_INPUT[MEDALLON_INPUT_CONTRACT]
-TABLE_INPUT_NAME = DATABASE_INPUT_CONTRACT["tableName"]
-TABLE_INPUT_SCHEMA = DATABASE_INPUT_CONTRACT["schema"]
-TABLE_INPUT_PATH = ".".join([TABLE_INPUT_SCHEMA, TABLE_INPUT_NAME])
-YEAR = DATABASE_INPUT_CONTRACT["queryYear"]
+TABLE_INPUT_PATH = ".".join(
+    [DATABASE_INPUT_CONTRACT["schema"], DATABASE_INPUT_CONTRACT["tableName"]]
+)
 
 MEDALLON_OUTPUT_CONTRACT_PONNOT = "bronze"
 CONTRACT_OUTPUT_PONNOT = get_contract("contract_aneel_companies_ponnot.yaml")
@@ -58,7 +59,7 @@ def load_aneel_ids() -> pd.DataFrame:
     df = conn.query_database(
         f"""
             SELECT * FROM {TABLE_INPUT_PATH} 
-            WHERE year = '{YEAR}' 
+            WHERE year = '{DATABASE_INPUT_CONTRACT["queryYear"]}' 
             AND company NOT LIKE '%tab%' 
             AND title LIKE '%_V%'
         """
@@ -92,26 +93,55 @@ def download_aneel_company_files(df_aneel_ids: pd.DataFrame):
     download_files(df_aneel_ids)
 
 
-# @save_parquet_decorator(MEDALLON_OUTPUT_CONTRACT, DATABASE_OUTPUT_CONTRACT)
-def read_aneel_company_files(row: pd.DataFrame):
+@save_parquet_decorator(
+    MEDALLON_OUTPUT_CONTRACT_PONNOT, DATABASE_OUTPUT_CONTRACT_PONNOT
+)
+def read_aneel_ponnot(row_title: str, **kwargs) -> gpd.GeoDataFrame:
+    """
+    Reads ANEEL PONNOT files and save it.
+
+    This function reads the downloaded ANEEL PONNOT files.
+    """
+    reader = Reader(DATABASE_OUTPUT_CONTRACT_PONNOT)
+    df_ponnot = reader.read_geofile(
+        "/".join([PATH_OUTPUT_PONNOT, row_title]),
+        driver="FileGDB",
+        layer="PONNOT",
+    )
+    return df_ponnot
+
+
+@save_parquet_decorator(MEDALLON_OUTPUT_CONTRACT_UCBT, DATABASE_OUTPUT_CONTRACT_UCBT)
+def read_aneel_ucbt(row_title: str, **kwargs) -> gpd.GeoDataFrame:
+    """
+    Reads ANEEL UCBT files and save it.
+
+    This function reads the downloaded ANEEL UCBT files.
+    """
+    reader = Reader(DATABASE_OUTPUT_CONTRACT_UCBT)
+    df_ucbt = reader.read_geofile(
+        "/".join([PATH_OUTPUT_UCBT, row_title]),
+        driver="FileGDB",
+        layer="UCBT_tab",
+    )
+    df_ucbt = df_ucbt.drop(columns=["geometry"])
+    return df_ucbt
+
+
+def read_aneel_company_files(row_title: str):
     """
     Reads ANEEL company files.
 
     This function reads the downloaded ANEEL company files.
     """
-    reader = Reader(DATABASE_OUTPUT_CONTRACT_PONNOT)
-    df_ponnot = reader.read_geofile(
-        "/".join([PATH_OUTPUT_PONNOT, row["title"].squeeze()]),
-        driver="FileGDB",
-        layer="PONNOT",
-    )
-    reader = Reader(DATABASE_OUTPUT_CONTRACT_UCBT)
-    df_ucbt = reader.read_geofile(
-        "/".join([PATH_OUTPUT_UCBT, row["title"].squeeze()]),
-        driver="FileGDB",
-        layer="UCBT_tab",
-    )
-    return df_ponnot, df_ucbt
+    kwargs = {"filename": "_".join([row_title.split(".")[0], "ponnot"])}
+    df_ponnot = read_aneel_ponnot(row_title, **kwargs)
+    del df_ponnot
+    gc.collect()
+    kwargs = {"filename": "_".join([row_title.split(".")[0], "ucbt"])}
+    df_ucbt = read_aneel_ucbt(row_title, **kwargs)
+    del df_ucbt
+    gc.collect()
 
 
 def main(download: bool = False):
@@ -124,4 +154,5 @@ def main(download: bool = False):
     df_aneel_ids = load_aneel_ids()
     if download:
         download_aneel_company_files(df_aneel_ids)
-    # df_ponnot = read_aneel_company_files(df_aneel_ids)
+    for row_title in tqdm(df_aneel_ids["title"]):
+        read_aneel_company_files(row_title)
