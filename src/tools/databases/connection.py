19c919f49e5c3bc0c5d059d6730a3c65278784c9
Yan Prada <yanpradamoro@gmail.com>
2024-04-05 08:24:11 -0300

    :sparkles: feat (connection): create new connection class DBConnection

--- src/tools/databases/connection.py
+++ src/tools/databases/connection.py
@@ -11,6 +11,7 @@ from sqlalchemy import create_engine, text, schema
 from sqlalchemy.orm import sessionmaker
 import pandas as pd
 import sqlalchemy
+from psycopg2.errors import UniqueViolation
 
 # Database credentials
 DB_USER = config("DB_USER")
@@ -72,6 +73,27 @@ class DBConnectionHandler:
         """
         return self.__engine
 
+
+class DBConnection(DBConnectionHandler):
+    """
+    A class representing a database connection.
+
+    Args:
+        database (str): The name of the database.
+
+    Attributes:
+        __engine (sqlalchemy.engine.Engine): The database engine.
+
+    Methods:
+        __create_schema: Create a schema in the database.
+        __add_pk_to_table: Add a primary key to a table in the database.
+        __add_fk_to_table: Add foreign keys to a table in the database.
+        __save_as_postgis: Save a table as a PostGIS table in the database.
+        __save_to_sql: Save a table as a SQL table in the database.
+        add_table: Adds a table to the database.
+        query_database: Executes a query on the database and returns the result as a DataFrame.
+    """
+
     def __create_schema(
         self, conn: sqlalchemy.engine.Connection, schema_name: str
     ) -> None:
@@ -100,11 +122,20 @@ class DBConnectionHandler:
             schema_name (str): The name of the schema containing the table.
             table_name (str): The name of the table to which the primary key will be added.
         """
-        conn.execute(
+        result = conn.execute(
             text(
-                f"ALTER TABLE {schema_name}.{table_name} ADD PRIMARY KEY ({primary_key})"
+                f"""SELECT constraint_name 
+                FROM information_schema.table_constraints
+                WHERE table_name = '{table_name}' 
+                AND constraint_type = 'PRIMARY KEY'"""
             )
         )
+        if not result.fetchone():
+            conn.execute(
+                text(
+                    f"""ALTER TABLE {schema_name}.{table_name} ADD PRIMARY KEY ({primary_key})"""
+                )
+            )
 
     def __add_fk_to_table(
         self,
@@ -124,13 +155,78 @@ class DBConnectionHandler:
                                 and the path of the foreging key in the database.
         """
         for fk_col, fk_path in foreign_keys:
-            conn.execute(
+            result = conn.execute(
                 text(
-                    f"""ALTER TABLE {schema_name}.{table_name} 
-                        ADD FOREIGN KEY ({fk_col}) 
-                        REFERENCES {fk_path}"""
+                    f"""SELECT constraint_name 
+                        FROM information_schema.table_constraints 
+                        WHERE table_name = '{table_name}' 
+                        AND constraint_type = 'FOREIGN KEY'"""
                 )
             )
+            if not result.fetchone():
+                conn.execute(
+                    text(
+                        f"""ALTER TABLE {schema_name}.{table_name} 
+                            ADD FOREIGN KEY ({fk_col}) 
+                            REFERENCES {fk_path}"""
+                    )
+                )
+
+    def __save_as_postgis(
+        self,
+        table: pd.DataFrame,
+        names: tuple[str, str],
+        conn: sqlalchemy.engine.Connection,
+        action_if_exists: str,
+    ):
+        """
+        Save a table as a PostGIS table in the database.
+
+        Args:
+            table (pd.DataFrame): The table to be saved.
+            names (tuple[str, str]): A tuple containing the schema name and table name.
+            conn (sqlalchemy.engine.Connection): The connection to the database.
+            action_if_exists (str): The action to take if the table already exists in the database.
+        """
+        schema_name, table_name = names
+        try:
+            table.to_postgis(
+                table_name,
+                conn,
+                schema=schema_name,
+                if_exists=action_if_exists,
+                index=False,
+            )
+        except UniqueViolation:
+            pass
+
+    def __save_to_sql(
+        self,
+        table: pd.DataFrame,
+        names: tuple[str, str],
+        conn: sqlalchemy.engine.Connection,
+        action_if_exists: str,
+    ):
+        """
+        Save a table as a SQL table in the database.
+
+        Args:
+            table (pd.DataFrame): The table to be saved.
+            names (tuple[str, str]): A tuple containing the schema name and table name.
+            conn (sqlalchemy.engine.Connection): The connection to the database.
+            action_if_exists (str): The action to take if the table already exists in the database.
+        """
+        schema_name, table_name = names
+        try:
+            table.to_sql(
+                table_name,
+                conn,
+                schema=schema_name,
+                if_exists=action_if_exists,
+                index=False,
+            )
+        except UniqueViolation:
+            pass
 
     def add_table(self, table: pd.DataFrame, database_contract: dict):
         """
@@ -144,6 +240,7 @@ class DBConnectionHandler:
         None
         """
         table_name = database_contract["tableName"]
+
         primary_key = next(
             (
                 col["column"]
@@ -161,22 +258,12 @@ class DBConnectionHandler:
         action_if_exists = database_contract["ifExists"]
         with self.__engine.begin() as conn:
             self.__create_schema(conn, schema_name)
+            args = (table, (schema_name, table_name), conn, action_if_exists)
             if table.filter(regex="geom").shape[1] > 0:
-                table.to_postgis(
-                    table_name,
-                    conn,
-                    schema=schema_name,
-                    if_exists=action_if_exists,
-                    index=False,
-                )
+                self.__save_as_postgis(*args)
             else:
-                table.to_sql(
-                    table_name,
-                    conn,
-                    schema=schema_name,
-                    if_exists=action_if_exists,
-                    index=False,
-                )
+                self.__save_to_sql(*args)
+        with self.__engine.begin() as conn:
             if primary_key is not None:
                 self.__add_pk_to_table(conn, schema_name, table_name, primary_key)
             if len(foreign_keys) > 0:
