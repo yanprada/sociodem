594af601bd785bb85aaa535650e199936ed3ab16
Yan Prada <yanpradamoro@gmail.com>
2024-04-05 18:35:05 -0300

    :sparkles: feat (make aneel): add paralelism and remove download functions to make dataset aneel

--- src/databases/bronze/aneel/make_dataset_aneel_companies.py
+++ src/databases/bronze/aneel/make_dataset_aneel_companies.py
@@ -13,42 +13,41 @@ Usage:
 
 """
 
+import os
 import gc
 import logging
+import multiprocessing
+import concurrent.futures
 import pandas as pd
 import geopandas as gpd
 from tqdm import tqdm
 
-from src.tools.utils.config import get_contract
 from src.tools.databases.connection import DBConnection
-from src.tools.databases.data_request.drivers.http_requester import HttpRequesterAneel
-from src.tools.utils.save import save_parquet_decorator
 from src.tools.utils.read import Reader
+from src.tools.utils.config import get_contract
+from src.tools.utils.save import save_parquet_decorator
+
 
 logging.basicConfig(level=logging.INFO)
 
-CONTRACT_INPUT = get_contract("contract_aneel_companies_id.yaml", "silver")
-TABLE_INPUT_PATH = ".".join([CONTRACT_INPUT["schema"], CONTRACT_INPUT["tableName"]])
-
-
+CONTRACT_ID = get_contract("contract_aneel_companies_id.yaml", "silver")
 CONTRACT_PONNOT = get_contract("contract_aneel_companies_ponnot.yaml", "bronze")
-
-
 CONTRACT_UCBT = get_contract("contract_aneel_companies_ucbt.yaml", "bronze")
+CONTRACT_RAMLIG = get_contract("contract_aneel_companies_ramlig.yaml", "bronze")
 
 
 def load_aneel_ids() -> pd.DataFrame:
-    """
-    Loads ANEEL IDs from a CSV file and returns them as a pandas DataFrame.
+    """Loads ANEEL IDs from a CSV file and returns them as a pandas DataFrame.
 
     Returns:
         pd.DataFrame: A DataFrame containing ANEEL IDs.
     """
     conn = DBConnection("silver")
+    path = ".".join([CONTRACT_ID["schema"], CONTRACT_ID["tableName"]])
     df = conn.query_database(
         f"""
-            SELECT * FROM {TABLE_INPUT_PATH} 
-            WHERE year = '{CONTRACT_INPUT["queryYear"]}' 
+            SELECT * FROM {path} 
+            WHERE year = '{CONTRACT_ID["queryYear"]}' 
             AND company NOT LIKE '%tab%' 
             AND title LIKE '%_V%'
         """
@@ -56,33 +55,7 @@ def load_aneel_ids() -> pd.DataFrame:
     return df
 
 
-def download_files(df: pd.DataFrame) -> None:
-    """
-    Downloads files from the ANEEL website.
-
-    Args:
-        df (pd.DataFrame): The DataFrame containing the ANEEL IDs.
-
-    """
-    aneel_request = HttpRequesterAneel()
-    aneel_request.request_from_page(
-        df["id"],
-        df["title"],
-        CONTRACT_PONNOT["physicalPath"],
-    )
-
-
-def download_aneel_company_files(df_aneel_ids: pd.DataFrame) -> None:
-    """
-    Downloads ANEEL company files.
-
-    This function loads ANEEL IDs and downloads the corresponding files for each company.
-    """
-
-    download_files(df_aneel_ids)
-
-
-@save_parquet_decorator(medallon="bronze", contract=CONTRACT_PONNOT)
+@save_parquet_decorator(medallon="bronze", contract=CONTRACT_PONNOT, save_db=False)
 def read_aneel_ponnot(row_title: str, **kwargs) -> gpd.GeoDataFrame:
     """
     Reads ANEEL PONNOT files and save it.
@@ -90,15 +63,18 @@ def read_aneel_ponnot(row_title: str, **kwargs) -> gpd.GeoDataFrame:
     This function reads the downloaded ANEEL PONNOT files.
     """
     reader = Reader(CONTRACT_PONNOT)
+    path = os.path.join(
+        CONTRACT_PONNOT["physicalPath"].replace("ponnot", "zip_files"), row_title
+    )
     df_ponnot = reader.read_geofile(
-        "/".join([CONTRACT_PONNOT["physicalPath"], row_title]),
+        file_path=path,
         driver="FileGDB",
         layer="PONNOT",
     )
     return df_ponnot
 
 
-@save_parquet_decorator(medallon="bronze", contract=CONTRACT_UCBT)
+@save_parquet_decorator(medallon="bronze", contract=CONTRACT_UCBT, save_db=False)
 def read_aneel_ucbt(row_title: str, **kwargs) -> gpd.GeoDataFrame:
     """
     Reads ANEEL UCBT files and save it.
@@ -106,13 +82,36 @@ def read_aneel_ucbt(row_title: str, **kwargs) -> gpd.GeoDataFrame:
     This function reads the downloaded ANEEL UCBT files.
     """
     reader = Reader(CONTRACT_UCBT)
+    path = os.path.join(
+        CONTRACT_UCBT["physicalPath"].replace("ucbt", "zip_files"),
+        row_title,
+    )
     df_ucbt = reader.read_geofile(
-        "/".join([CONTRACT_UCBT["physicalPath"], row_title]),
+        file_path=path,
         driver="FileGDB",
         layer="UCBT_tab",
     )
     df_ucbt = df_ucbt.drop(columns=["geometry"])
     return df_ucbt
+
+
+@save_parquet_decorator(medallon="bronze", contract=CONTRACT_RAMLIG, save_db=False)
+def read_aneel_ramlig(row_title: str, **kwargs) -> gpd.GeoDataFrame:
+    """
+    Reads ANEEL RAMLIG files and save it.
+
+    This function reads the downloaded ANEEL RAMLIG files.
+    """
+    reader = Reader(CONTRACT_RAMLIG)
+    path = os.path.join(
+        CONTRACT_RAMLIG["physicalPath"].replace("ramlig", "zip_files"), row_title
+    )
+    df_ramlig = reader.read_geofile(
+        file_path=path,
+        driver="FileGDB",
+        layer="RAMLIG",
+    )
+    return df_ramlig
 
 
 def read_aneel_company_files(row_title: str):
@@ -121,26 +120,32 @@ def read_aneel_company_files(row_title: str):
 
     This function reads the downloaded ANEEL company files.
     """
-    kwargs = {"filename": "_".join([row_title.split(".")[0], "ponnot"])}
-    df_ponnot = read_aneel_ponnot(row_title, **kwargs)
-    del df_ponnot
-    gc.collect()
-    kwargs = {"filename": "_".join([row_title.split(".")[0], "ucbt"])}
-    df_ucbt = read_aneel_ucbt(row_title, **kwargs)
-    del df_ucbt
-    gc.collect()
+    if not os.path.exists(os.path.join(CONTRACT_PONNOT["physicalPath"], row_title)):
+        kwargs = {"filename": "_".join([row_title.split(".")[0], "ponnot"])}
+        df_ponnot = read_aneel_ponnot(row_title, **kwargs)
+        del df_ponnot
+        gc.collect()
+    if not os.path.exists(os.path.join(CONTRACT_UCBT["physicalPath"], row_title)):
+        kwargs = {"filename": "_".join([row_title.split(".")[0], "ucbt"])}
+        df_ucbt = read_aneel_ucbt(row_title, **kwargs)
+        del df_ucbt
+        gc.collect()
+    if not os.path.exists(os.path.join(CONTRACT_RAMLIG["physicalPath"], row_title)):
+        kwargs = {"filename": "_".join([row_title.split(".")[0], "ramlig"])}
+        df_ramlig = read_aneel_ramlig(row_title, **kwargs)
+        del df_ramlig
+        gc.collect()
 
 
-def main(download: bool = False):
+def main():
     """
     Main function for making dataset for ANEEL companies.
-
-    Args:
-        download (bool): Flag indicating whether to download ANEEL company files.
     """
     df_aneel_ids = load_aneel_ids()
-    if download:
-        download_aneel_company_files(df_aneel_ids)
-    for row_title in tqdm(df_aneel_ids["title"]):
-        logging.info("Reading %s", row_title)
-        read_aneel_company_files(row_title)
+    num_cores = multiprocessing.cpu_count()
+    with concurrent.futures.ProcessPoolExecutor(max_workers=num_cores) as executor:
+        futures = [
+            executor.submit(read_aneel_company_files, row_title)
+            for row_title in tqdm(df_aneel_ids["title"])
+        ]
+        concurrent.futures.wait(futures)
