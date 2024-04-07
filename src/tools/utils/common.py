23a516d4b33d6e22cd940a2b366cf9ef0749389a
Yan Prada <yanpradamoro@gmail.com>
2024-04-07 14:50:21 -0300

    :sparkles: feat (common): add common functions in utils tools

---
+++ src/tools/utils/common.py
@@ -0,0 +1,62 @@
+"""
+This module contains utility functions for common tasks.
+
+Functions:
+- write_log(message, level="info"): Write a log message with the specified level and timestamp.
+- get_column_memory_usage(df): Calculate the memory usage of each column in a DataFrame.
+"""
+
+import logging
+import datetime
+
+
+def write_log(message, level="info"):
+    """
+    Write a log message with the specified level and timestamp.
+
+    Parameters:
+    message (str): The log message to be written.
+    level (str): The log level, either 'info' or 'error'. Default is 'info'.
+
+    Returns:
+    None
+    """
+    # Set up logging configuration
+    logging.basicConfig(
+        level=logging.INFO if level == "info" else logging.ERROR,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+    # Get current timestampy
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    # Write log message
+    if level == "info":
+        logging.info("%s - %s", timestamp, message)
+    else:
+        logging.error("%s - %s", timestamp, message)
+
+
+def get_column_memory_usage(df):
+    """
+    Calculate the memory usage of each column in a DataFrame.
+
+    Parameters:
+    df (pandas.DataFrame): The DataFrame for which to calculate the memory usage.
+
+    Returns:
+    dict: A dictionary where the keys are column names and the values are
+    the memory usage of each column.
+    """
+    column_memory_usage = {}
+    for column in df.columns:
+        column_memory_usage[column] = round(
+            df[column].memory_usage(deep=True) / 1024, 2
+        )
+    column_memory_usage = dict(
+        sorted(column_memory_usage.items(), key=lambda x: x[1], reverse=True)
+    )
+    for column, memory_usage in column_memory_usage.items():
+        column_memory_usage[column] = str(memory_usage) + " Kb"
+    return column_memory_usage
