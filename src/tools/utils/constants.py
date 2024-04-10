12235f42441b03199f5cfaac892bebe86f502421
Yan Prada <yanpradamoro@gmail.com>
2024-04-10 08:49:27 -0300

    :sparkles: feat (constants): add constants file to project

---
+++ src/tools/utils/constants.py
@@ -0,0 +1,38 @@
+"""
+This module contains constants used in the sociodem project.
+
+- CRS: The coordinate reference system used in the project.
+- STATES: A list of Brazilian states.
+"""
+
+CRS = "EPSG:4674"
+
+STATES = [
+    "AC",
+    "AL",
+    "AP",
+    "AM",
+    "BA",
+    "CE",
+    "DF",
+    "ES",
+    "GO",
+    "MA",
+    "MT",
+    "MS",
+    "MG",
+    "PA",
+    "PB",
+    "PR",
+    "PE",
+    "PI",
+    "RJ",
+    "RN",
+    "RS",
+    "RO",
+    "RR",
+    "SC",
+    "SP",
+    "SE",
+    "TO",
+]
