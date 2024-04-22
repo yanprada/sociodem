73f6c8e5ecf957a8623b4cc964659cc44ba25184
Yan Prada <yanpradamoro@gmail.com>
2024-04-22 20:41:05 -0300

    :sparkles: feat (common): add data consistency function to check number of partitions of a file

--- src/tools/utils/common.py
+++ src/tools/utils/common.py
@@ -6,13 +6,18 @@ Functions:
 - get_column_memory_usage(df): Calculate the memory usage of each column in a DataFrame.
 """
 
+import os
 import logging
+from src.tools.utils.config import get_contract
+
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
 )
+
+CONTRACT_PARTITIONS = get_contract("contract_partitions.yaml")
 
 
 def write_log(message, level="info"):
@@ -54,3 +59,27 @@ def get_column_memory_usage(df):
     for column, memory_usage in column_memory_usage.items():
         column_memory_usage[column] = str(memory_usage) + " Kb"
     return column_memory_usage
+
+
+def check_data_consistency(path) -> dict:
+    """
+    Check the consistency of data in the given path.
+
+    Args:
+        path (str): The path to check for data consistency.
+
+    Returns:
+        dict: A dictionary containing subfolders as keys and a boolean value indicating
+              whether the number of files in the subfolder matches the expected number
+              of files.
+
+    """
+    subfolders = [
+        subfolder for subfolder in CONTRACT_PARTITIONS.keys() if path in subfolder
+    ]
+    data_wrong = {}
+    for subfolder in subfolders:
+        expected_num_files = CONTRACT_PARTITIONS[subfolder]
+        file_count = len(os.listdir(subfolder))
+        data_wrong[subfolder] = expected_num_files > file_count
+    return data_wrong
