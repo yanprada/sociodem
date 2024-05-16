64d6c5ef4c8c47079166331af1f9ea3949cd98f4
Yan Prada <yanpradamoro@gmail.com>
2024-05-16 09:20:26 -0300

    :recycle: refactor (aneel ids): refactor code to new aneed ids dataframe

--- src/databases/bronze/aneel/download_aneel_companies.py
+++ src/databases/bronze/aneel/download_aneel_companies.py
@@ -11,7 +11,7 @@ from src.tools.utils.save import save_parquet_decorator
 from src.tools.utils.read import Reader
 from src.tools.databases.data_request.drivers.http_requester import HttpRequesterAneel
 
-CONTRACTS = get_aneel_contracts("bronze")
+CONTRACTS_BRONZE = get_aneel_contracts("bronze")
 
 
 def load_aneel_ids() -> pd.DataFrame:
@@ -21,45 +21,9 @@ def load_aneel_ids() -> pd.DataFrame:
     Returns:
         pd.DataFrame: A DataFrame containing ANEEL IDs.
     """
-    columns = [col["column"] for col in CONTRACTS["company_id"].columns]
-    reader = Reader(CONTRACTS["company_id"])
-    df = reader.read_csv(CONTRACTS["company_id"]["physicalPath"], usecols=columns)
-    return df
-
-
-def split_tags(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Splits the tags column into multiple columns.
-
-    Args:
-        df (pd.DataFrame): The DataFrame to split.
-
-    Returns:
-        pd.DataFrame: The DataFrame with the tags column split.
-    """
-    tags = (
-        df["tags"].str.split(",", expand=True).rename(columns={0: "company", 1: "year"})
-    )
-    df = pd.concat([df, tags], axis=1)
-    df = df.drop(columns=["tags", 2])
-    return df
-
-
-def download_files(df: pd.DataFrame) -> None:
-    """
-    Downloads files from the ANEEL website.
-
-    Args:
-        df (pd.DataFrame): The DataFrame containing the ANEEL IDs.
-
-    """
-    aneel_request = HttpRequesterAneel()
-    path = os.path.join(CONTRACTS["datalake"]["physicalPath"])
-    aneel_request.request_from_page(
-        df["id"],
-        df["title"],
-        path,
-    )
+    reader = Reader(CONTRACTS_BRONZE["company_id"])
+    df = reader.read_csv(CONTRACTS_BRONZE["company_id"]["physicalPath"])
+    return df.drop_duplicates()
 
 
 def download_aneel_company_files(df_aneel_ids: pd.DataFrame) -> None:
@@ -67,31 +31,36 @@ def download_aneel_company_files(df_aneel_ids: pd.DataFrame) -> None:
     Downloads ANEEL company files.
 
     This function loads ANEEL IDs and downloads the corresponding files for each company.
+
+    Args:
+        df_aneel_ids (pd.DataFrame): A DataFrame containing ANEEL company IDs.
+
     """
 
-    download_files(df_aneel_ids)
+    aneel_request = HttpRequesterAneel()
+    path = os.path.join(CONTRACTS_BRONZE["datalake"]["physicalPath"])
+    aneel_request.request_from_page(
+        df_aneel_ids["company_ids"],
+        path,
+    )
 
 
-@save_parquet_decorator(medallon="silver", contract=CONTRACTS["company_id"])
+@save_parquet_decorator(medallon="bronze", contract=CONTRACTS_BRONZE["company_id"])
 def main() -> pd.DataFrame:
     """
     This function loads ANEEL IDs, splits tags, and returns a DataFrame filtered by selected year.
 
     Example:
         >>> main()
-               id         title                            company    year
-        0      123456789  SSDAT_2022-12-31.gdb.zip         SSDAT      2023
-        1      987654321  PONNOT_2022-12-31.gdb.zip        PONNOT     2023
-        2      456789123  COORSEL_7016-12-31_V11.gdb.zip   COORSEL    2023
+               id
+        0      123456789
+        1      987654321
+        2      456789123
 
     Returns:
         pandas.DataFrame: A DataFrame containing ANEEL Company IDs filtered by selected year.
 
     """
-    year = CONTRACTS["company_id"]["queryYear"]
-    df = load_aneel_ids().pipe(split_tags).query(f"year == '{year}'").drop_duplicates()
-    df["company_id"] = df.apply(
-        lambda row: row["title"].replace(row["company"], "cp").split("_")[1], axis=1
-    )
+    df = load_aneel_ids()
     download_aneel_company_files(df)
     return df
