eb5a76b93f388854aae8dfb89dcd10e4b3d0d62d
Yan Prada <yanpradamoro@gmail.com>
2024-05-25 19:40:13 -0300

    :recycle: refactor (censo bronze): add municipality

--- src/databases/bronze/censo/make_bronze_dataset_censo.py
+++ src/databases/bronze/censo/make_bronze_dataset_censo.py
@@ -16,93 +16,17 @@ import zipfile
 import pandas as pd
 from tqdm import tqdm
 from src.tools.utils.read import Reader
-from src.tools.data_contract.aneel_data_contract import get_contract
+from src.tools.data_contract.censo_data_contract import get_censo_contracts
+from src.tools.data_contract.validation_data_contract import get_validation_partitions
 from src.tools.utils.save import save_parquet_decorator
 from src.tools.utils.constants import STATES
 from src.tools.utils.common import write_log, check_data_consistency
 
-CONTRACT_MUN_CENSO_2010 = get_contract("censo/contract_mun_censo_2010.yaml", "bronze")
-
-CONTRACT_SECTORS_CENSO_2010 = get_contract(
-    "censo/contract_sectors_censo_2010.yaml", "bronze"
-)
-CONTRACT_SECTORS_CENSO_2022 = get_contract(
-    "censo/contract_sectors_censo_2022.yaml", "bronze"
-)
-CONTRACT_DOMPP_CENSO_2022 = get_contract(
-    "censo/contract_dompp_censo_2022.yaml", "bronze"
-)
-CONTRACT_PARTITIONS = get_contract("contract_partitions.yaml")
-CONTRACTS = {
-    "municipalities_2010": CONTRACT_MUN_CENSO_2010,
-    "sectors_2010": CONTRACT_SECTORS_CENSO_2010,
-    "sectors_2022": CONTRACT_SECTORS_CENSO_2022,
-    "dompp_2022": CONTRACT_DOMPP_CENSO_2022,
-}
+CONTRACT_PARTITIONS = get_validation_partitions()
+CONTRACTS = get_censo_contracts("bronze")
 
 
-def get_censo_data(contract_key):
-    """
-    Retrieves the data from the Censo dataset.
-
-    Args:
-        contract_key (str): The key of the contract to use.
-
-    Returns:
-        pandas.DataFrame: A DataFrame containing the data.
-    """
-    reader = Reader(CONTRACTS[contract_key])
-    dfs = []
-    for state in tqdm(STATES):
-        filepath = os.path.join(
-            CONTRACTS[contract_key]["physicalPath"]
-            .replace("databases", "datalake")
-            .replace("bronze/", ""),
-            "".join([state, ".zip"]),
-        )
-        df = reader.read_geofile(filepath)
-        dfs.append(df)
-    dfs = pd.concat(dfs)
-    return dfs
-
-
-@save_parquet_decorator(medallon="bronze", contract=CONTRACT_MUN_CENSO_2010)
-def get_municipalities_2010(**kwargs):
-    """
-    Retrieves the municipalities data from the Censo 2010 dataset.
-
-    Returns:
-        pandas.DataFrame: A DataFrame containing the municipalities data.
-    """
-    dfs = get_censo_data("municipalities_2010")
-    return dfs
-
-
-@save_parquet_decorator(medallon="bronze", contract=CONTRACT_SECTORS_CENSO_2010)
-def get_sectors_2010(**kwargs):
-    """
-    Retrieves the census sectors data from the Censo 2010 dataset.
-
-    Returns:
-        pandas.DataFrame: A DataFrame containing the census sectors data.
-    """
-    dfs = get_censo_data("sectors_2010")
-    return dfs
-
-
-@save_parquet_decorator(medallon="bronze", contract=CONTRACT_SECTORS_CENSO_2022)
-def get_sectors_2022(**kwargs):
-    """
-    Retrieves the census sectors data from the Censo 2022 dataset.
-
-    Returns:
-        pandas.DataFrame: A DataFrame containing the census sectors data.
-    """
-    dfs = get_censo_data("sectors_2022")
-    return dfs
-
-
-@save_parquet_decorator(medallon="bronze", contract=CONTRACT_DOMPP_CENSO_2022)
+@save_parquet_decorator(medallon="bronze", contract=CONTRACTS["dompp_2022"])
 def get_dompp_per_state_2022(state, **kwargs):
     """
     Retrieves the DOMPP data for a specific state in 2022.
@@ -118,9 +42,9 @@ def get_dompp_per_state_2022(state, **kwargs):
         FileNotFoundError: If the file for the specified state is not found.
 
     """
-    reader = Reader(CONTRACT_DOMPP_CENSO_2022)
+    reader = Reader(CONTRACTS["dompp_2022"])
     filepath = os.path.join(
-        CONTRACT_DOMPP_CENSO_2022["physicalPath"]
+        CONTRACTS["dompp_2022"]["physicalPath"]
         .replace("databases", "datalake")
         .replace("bronze/", ""),
         "".join([state, ".zip"]),
@@ -147,59 +71,6 @@ def get_dompp_2022():
         _ = get_dompp_per_state_2022(state, **kwargs)
 
 
-def upload_municipalities_2010():
-    """
-    Uploads municipalities data for the year 2010.
-
-    This function processes the municipalities data and checks if the data already exists.
-    If the data does not exist, it calls the `get_municipalities_2010` function to retrieve it.
-    """
-    write_log("Processing municipalities data...")
-    kwargs = {"filename": "municipalities"}
-    if os.path.exists(
-        os.path.join(CONTRACT_MUN_CENSO_2010["physicalPath"], "municipalities.parquet")
-    ):
-        write_log("Municipalities data already exists.")
-    else:
-        _ = get_municipalities_2010(**kwargs)
-
-
-def upload_sectors_2010():
-    """
-    Uploads sectors data for the year 2010.
-
-    This function processes sectors data and checks if the data already exists.
-    If the data does not exist, it calls the `get_sectors_2010` function to retrieve it.
-    """
-    write_log("Processing sectors data...")
-    kwargs = {"filename": ""}
-    path = CONTRACT_SECTORS_CENSO_2010["physicalPath"]
-    if os.path.exists(path):
-        data_wrong = check_data_consistency(path)
-        if any(data_wrong.values()):
-            _ = get_sectors_2010(**kwargs)
-    else:
-        _ = get_sectors_2010(**kwargs)
-
-
-def upload_sectors_2022():
-    """
-    Uploads sectors data for the year 2022.
-
-    This function processes sectors data and checks if the data already exists.
-    If the data does not exist, it calls the `get_sectors_2022` function to retrieve it.
-    """
-    write_log("Processing sectors data...")
-    kwargs = {"filename": ""}
-    path = CONTRACT_SECTORS_CENSO_2022["physicalPath"]
-    if os.path.exists(path):
-        data_wrong = check_data_consistency(path)
-        if any(data_wrong.values()):
-            _ = get_sectors_2022(**kwargs)
-    else:
-        _ = get_sectors_2022(**kwargs)
-
-
 def upload_dompp_2022():
     """
     Uploads DOMPP data for the year 2022.
@@ -208,7 +79,7 @@ def upload_dompp_2022():
     If the data does not exist, it calls the `get_dompp_2022` function to retrieve it.
     """
     write_log("Processing dompp data...")
-    path = CONTRACT_DOMPP_CENSO_2022["physicalPath"]
+    path = CONTRACTS["dompp_2022"]["physicalPath"]
     if os.path.exists(path):
         data_wrong = check_data_consistency(path)
         if any(data_wrong.values()):
@@ -217,11 +88,62 @@ def upload_dompp_2022():
         get_dompp_2022()
 
 
+def upload_censo_data(layer_key):
+    """
+    Uploads municipalities data for the year 2010.
+
+    This function processes the municipalities data and checks if the data already exists.
+    If the data does not exist, it calls the `get_municipalities_2010` function to retrieve it.
+    """
+
+    @save_parquet_decorator(medallon="bronze", contract=CONTRACTS[layer_key])
+    def get_censo_data(layer_key, **kwargs):
+        """
+        Retrieves the data from the Censo dataset.
+
+        Args:
+            layer_key (str): The key of the layer to use.
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            pandas.DataFrame: A DataFrame containing the data.
+        """
+        reader = Reader(CONTRACTS[layer_key])
+        dfs = []
+        for state in tqdm(STATES):
+            filepath = os.path.join(
+                CONTRACTS[layer_key]["physicalPath"]
+                .replace("databases", "datalake")
+                .replace("bronze/", ""),
+                "".join([state, ".zip"]),
+            )
+            df = reader.read_geofile(filepath)
+            dfs.append(df)
+        dfs = pd.concat(dfs)
+        return dfs
+
+    write_log(f"Processing {layer_key} data...")
+    kwargs = {"filename": layer_key}
+    path = CONTRACTS[layer_key]["physicalPath"]
+    if os.path.exists(os.path.join(path, f"{layer_key}.parquet")):
+        data_wrong = check_data_consistency(path)
+        if any(data_wrong.values()):
+            _ = get_censo_data(layer_key, **kwargs)
+        write_log(f"{layer_key} data already exists.")
+    else:
+
+        _ = get_censo_data(layer_key, **kwargs)
+
+
 def main():
     """
     The main function that executes the script.
     """
-    upload_municipalities_2010()
-    upload_sectors_2010()
-    upload_sectors_2022()
+    for layer_key in [
+        "municipalities_2010",
+        "municipalities_2022",
+        "sectors_2010",
+        "sectors_2022",
+    ]:
+        upload_censo_data(layer_key)
     upload_dompp_2022()
