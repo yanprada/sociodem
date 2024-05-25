e3d2e45c97a4edcf29042f5a53aef0b10cb91563
Yan Prada <yanpradamoro@gmail.com>
2024-05-25 19:46:11 -0300

    :sparkles: feat (mapbiomas): add script to download mapbiomas from 2018 to 2022

---
+++ src/databases/bronze/mapbiomas/download_mapbiomas.py
@@ -0,0 +1,25 @@
+"""
+This script is used to download Mapbiomas data for a specified range of years.
+
+It imports the necessary modules and defines the CONTRACT variable, which contains the contract
+information for downloading Mapbiomas data. The main() function is the entry point of the script,
+which initializes an instance of HttpRequesterMapbiomas and makes a request to download data
+for the specified range of years using the contract defined in CONTRACT.
+"""
+
+from src.tools.databases.data_request.drivers.http_requester import (
+    HttpRequesterMapbiomas,
+)
+from src.tools.utils.data_contract import get_contract
+
+CONTRACT = get_contract("mapbiomas/contract_mapbiomas.yaml", "bronze")
+
+
+def main():
+    """
+    This function is the entry point of the script for downloading Mapbiomas data.
+    It initializes an instance of HttpRequesterMapbiomas and makes a request to download data
+    for the specified range of years using the contract defined in CONTRACT.
+    """
+    requester = HttpRequesterMapbiomas()
+    requester.request_from_page(range(2018, 2023), CONTRACT["physicalPath"])
