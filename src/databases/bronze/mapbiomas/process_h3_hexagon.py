4ee79a0ef6bb5c5b0047bc418f75cc853929ea12
Yan Prada <yanpradamoro@gmail.com>
2024-07-10 09:01:29 -0300

    :sparkles: feat (mapbiomas h3 paralel): add bronze h3 index mapbiomas

--- src/databases/bronze/mapbiomas/process_h3_hexagon.py
+++ src/databases/bronze/mapbiomas/process_h3_hexagon.py
@@ -13,6 +13,7 @@ from concurrent.futures import ProcessPoolExecutor, as_completed
 import os
 from tqdm import tqdm
 import h3
+import mlflow
 
 from src.tools.utils.constants import HEX_RESOLUTION
 from src.tools.utils.read import Reader
@@ -21,6 +22,7 @@ from src.tools.data_contract.mapbiomas_data_contract import get_mapbiomas_contra
 from src.tools.utils.common import count_files
 
 CONTRACT = get_mapbiomas_contracts("bronze")
+mlflow.set_experiment("mapbiomas bronze")
 
 
 @save_parquet_decorator("bronze", CONTRACT["mapbiomas_2022"], save_pq=False)
@@ -54,7 +56,41 @@ def process_partition(partition: int, folder_path: str):
         The processed data.
 
     """
-    return load_data(partition, folder_path)
+    df = load_data(partition, folder_path)
+    temp = df.groupby(["hex_col"])["size"].sum()
+    with mlflow.start_run(run_name=str(partition)):
+        mlflow.log_metric("num_hex", df["hex_col"].nunique())
+        mlflow.log_metric("num_points", df["size"].sum())
+        mlflow.log_metric("mean_points_per_hex", temp.mean())
+        mlflow.log_metric("std_point_per_hex", temp.std())
+    return df
+
+
+def run_process(folder_path: str, start: int, end: int):
+    """
+    Run the process to process H3 hexagon data in parallel.
+
+    Args:
+        folder_path (str): The path to the folder containing the data files.
+        start (int): The starting index of the data files to process.
+        end (int): The ending index of the data files to process.
+    """
+    num_files = end - start
+    with ProcessPoolExecutor() as executor:
+        futures = [
+            executor.submit(process_partition, partition, folder_path)
+            for partition in range(start, end)
+        ]
+        for future in tqdm(
+            as_completed(futures),
+            total=num_files,
+            desc="Processing H3 hexagon data batch",
+        ):
+            try:
+                _ = future.result()
+
+            except Exception as e:
+                print(e)
 
 
 def main():
@@ -67,16 +103,11 @@ def main():
     """
     folder_path = CONTRACT["mapbiomas_2022"]["physicalPath"]
     num_files = count_files(folder_path)
-
-    with ProcessPoolExecutor() as executor:
-        futures = [
-            executor.submit(process_partition, partition, folder_path)
-            for partition in range(num_files)
-        ]
-        for future in tqdm(
-            as_completed(futures), total=num_files, desc="Processing H3 hexagon data"
-        ):
-            _ = future.result()  # Process the result if needed
+    start = 0
+    batch = 100
+    for i in tqdm(range(start, num_files, batch), desc="Processing h3 data"):
+        end = i + batch if i + batch < num_files else num_files
+        run_process(folder_path, i, end)
 
 
 if __name__ == "__main__":
