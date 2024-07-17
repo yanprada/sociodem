536f16c939b06cd5806b0e48a0c4668f19e3b049
Yan Prada <yanpradamoro@gmail.com>
2024-07-17 08:16:21 -0300

    :sparkles: feat (mapbiomas bronze): scripts to group mapbioma land use in h3 index hexagons

--- src/databases/bronze/mapbiomas/create_grouped_by_hex_mapbiomas.py
+++ src/databases/bronze/mapbiomas/create_grouped_by_hex_mapbiomas.py
@@ -12,15 +12,13 @@ from src.tools.data_contract.mapbiomas_data_contract import get_mapbiomas_contra
 CONTRACT_BRONZE = get_mapbiomas_contracts("bronze")
 
 
-def main():
+def create_grouped_by_hex_mapbiomas(conn: DBConnection) -> None:
     """
-    This function creates a new table in the 'silver' database by aggregating data
-    from the 'bronze' database.
-    It calculates the total size for each combination of 'hex_col' and 'value' columns
-    in the 'bronze' table,
-    and stores the result in the 'silver' table.
+    Creates a new table with grouped data based on hex_col and value columns from an existing table.
+
+    Args:
+        conn (DBConnection): The database connection object.
     """
-    conn = DBConnection("bronze")
     old_path = ".".join(
         [
             CONTRACT_BRONZE["mapbiomas_2022"]["schema"],
@@ -39,3 +37,42 @@ def main():
     GROUP BY hex_col, value;
     """
     conn.create_table_from_sql(query, new_path)
+
+
+def create_unique_hex_ids_mapbiomas(conn: DBConnection) -> None:
+    """
+    Create a table with unique hex IDs from the existing mapbiomas table.
+
+    Args:
+        conn (DBConnection): The database connection object.
+    """
+
+    old_path = ".".join(
+        [
+            CONTRACT_BRONZE["grouped_by_hex_mapbiomas_2022"]["schema"],
+            CONTRACT_BRONZE["grouped_by_hex_mapbiomas_2022"]["tableName"],
+        ]
+    )
+    query = f"""
+    SELECT DISTINCT hex_col
+    FROM {old_path}
+    """
+
+    schema = CONTRACT_BRONZE["unique_hex_ids"]["schema"]
+    table = CONTRACT_BRONZE["unique_hex_ids"]["tableName"]
+    new_path = ".".join([schema, table])
+    conn.create_table_from_sql(query, new_path)
+    conn.create_pk(schema, table, "col_id")
+
+
+def main() -> None:
+    """
+    This function creates a new table in the 'silver' database by aggregating data
+    from the 'bronze' database.
+    It calculates the total size for each combination of 'hex_col' and 'value' columns
+    in the 'bronze' table,
+    and stores the result in the 'silver' table.
+    """
+    conn = DBConnection("bronze")
+    create_grouped_by_hex_mapbiomas(conn)
+    create_unique_hex_ids_mapbiomas(conn)
