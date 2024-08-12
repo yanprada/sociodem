79337b3e6ce0ec506aa730d16ff141c3421cbaec
Yan Prada <yanpradamoro@gmail.com>
2024-08-12 08:56:12 -0300

    :sparkles: feat (pois silver): add execution manager for pois pipeline

---
+++ src/databases/silver/pois/config.py
@@ -0,0 +1,28 @@
+"""
+This module contains the configuration settings for the bronze 
+    database related to the Buildings data.
+
+Attributes:
+    EXECUTION_ID (None): The execution ID.
+    PARAMS (dict): A dictionary containing the parameters for the bronze database.
+        - "medallon" (str): The medallon value for the bronze database.
+        - "data_name" (str): The data name for the buildings data.
+
+"""
+
+import os
+
+from src.tools.data_contract.pois_data_contract import get_pois_contracts
+
+from config.run_mode import DEBUG
+
+EXECUTION_ID = None
+
+BASE_PARAMS = {
+    "medallon": "silver",
+    "data_name": "pois",
+    "config_path": os.path.abspath(__file__),
+    "data_contracts": [get_pois_contracts("bronze"), get_pois_contracts("silver")],
+    "run_mode": "single_file" if DEBUG else "pipeline",
+    "last_run": None,
+}
---
+++ src/databases/silver/pois/execution.py
@@ -0,0 +1,22 @@
+"""
+This module contains the pipeline for processing buildings data.
+"""
+
+from src.databases.silver.pois.config import EXECUTION_ID, BASE_PARAMS
+from src.tools.utils.execution_manager import ExecutionManager
+from src.databases.silver.pois.steps import a_create_places_hex
+from config.run_mode import DEBUG
+
+
+def main():
+    """
+    Main function for executing the silver Pois data pipeline.
+    """
+    mode = not DEBUG
+    BASE_PARAMS["execution_details"] = {
+        "steps": [
+            {"run": False, "function": a_create_places_hex.main},
+        ],
+    }
+    execution_manager = ExecutionManager(BASE_PARAMS)
+    execution_manager.start_execution(EXECUTION_ID, mode)
