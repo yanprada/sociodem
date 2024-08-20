25b83503a94ce01df799de08f179a1dc68a817b3
Yan Prada <yanpradamoro@gmail.com>
2024-08-20 08:14:14 -0300

    :sparkles: feat (loader): add places loader

--- src/tools/utils/loader.py
+++ src/tools/utils/loader.py
@@ -17,9 +17,10 @@ from src.tools.databases.data_connection.connection import DBConnection
 from src.tools.utils.constants import CRS_GLOBAL, CRS_IBGE
 from src.tools.utils.common import get_db_path
 from src.tools.data_contract.censo_data_contract import get_censo_contracts
-
+from src.tools.data_contract.pois_data_contract import get_pois_contracts
 
 CONTRACT_CENSO_BRONZE = get_censo_contracts("bronze")
+CONTRACT_POIS_BRONZE = get_pois_contracts("bronze")
 
 
 class Loader:
@@ -60,3 +61,17 @@ class Loader:
         conn = DBConnection("bronze")
         query = f"SELECT DISTINCT cod_mun FROM {path}"
         return conn.query_database(query)["cod_mun"].tolist()
+
+    @staticmethod
+    def get_places():
+        """
+        Retrieves a list of distinct municipality codes from the specified database table.
+
+        Returns:
+            list: A list of distinct municipality codes.
+        """
+        contract_dompp = CONTRACT_POIS_BRONZE["pois"]
+        path = get_db_path(contract_dompp)
+        conn = DBConnection("bronze")
+        query = f"SELECT cd_mun, nm_mun, general_category, hex_col FROM {path}"
+        return conn.query_database(query)
