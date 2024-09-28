f78b8cce7893727eb416eb8160bc53663f3418bf
Yan Prada <yanpradamoro@gmail.com>
2024-09-27 21:03:46 -0300

    :recycle: refactor (buildings): update buildings execution id

--- src/databases/bronze/buildings/google/config.py
+++ src/databases/bronze/buildings/google/config.py
@@ -13,16 +13,17 @@ Attributes:
 import os
 
 from src.tools.data_contract.buildings_data_contract import get_buildings_contracts
+from src.tools.data_contract.censo_data_contract import get_censo_contracts
 from config.run_mode import DEBUG
 
-EXECUTION_ID = "bronze-buildings-W6KKXJzPYgkpAW4"
-
+EXECUTION_ID = "bronze-buildings-PUjmWqWosFvYHbH"
 BASE_PARAMS = {
     "medallon": "bronze",
     "data_name": "buildings",
     "config_path": os.path.abspath(__file__),
     "data_contracts": {
         "raw_data": get_buildings_contracts("raw_data"),
+        "state_censo": get_censo_contracts("raw_data"),
         "bronze": get_buildings_contracts("bronze"),
     },
     "run_mode": "single_file" if DEBUG else "pipeline",
