70cccc6debe7d187b4c09de66025862f93e4eae7
Yan Prada <yanpradamoro@gmail.com>
2024-10-04 18:07:14 -0300

    :sparkles: feat (buildings): enhance memory efficience

--- src/databases/bronze/buildings/google/steps/b_process_image_to_hex.py
+++ src/databases/bronze/buildings/google/steps/b_process_image_to_hex.py
@@ -17,7 +17,6 @@ import logging
 from typing import List, Tuple
 import rasterio
 import numpy as np
-from tqdm import tqdm
 import mlflow
 import h3
 from dask.distributed import Client, LocalCluster, progress
@@ -49,6 +48,10 @@ mlflow.set_experiment(EXPERIMENT_ID)
 LOG_FILE_PATH = os.path.join(os.path.dirname(__file__), "failed_files.log")
 logging.basicConfig(filename=LOG_FILE_PATH, level=logging.ERROR)
 
+RUN_TIME = time.strftime("%Y-%m-%d %H:%M:%S")
+STATE = "SP"
+YEAR = BUILDING_CONTRACTS_RAW["buildings_google"]["physicalPath"].split("/")[-2]
+
 
 def log_failed_file(file_path: str) -> None:
     """Log a file as failed."""
@@ -56,13 +59,12 @@ def log_failed_file(file_path: str) -> None:
         log_file.write(f"{file_path}\n")
 
 
-def process_image(file_path: str) -> Tuple[np.ndarray, np.ndarray]:
+def process_image(file_path: str) -> gpd.GeoDataFrame:
     """
     Processes a GeoTIFF image file to extract building locations and their fractional counts.
     Reads the image in window chunks and skips windows without data.
     """
-    lats_lons_list = []
-    building_counts_list = []
+    result = []
     with rasterio.open(file_path) as src:
         crs = src.crs
         transformer = (
@@ -95,15 +97,9 @@ def process_image(file_path: str) -> Tuple[np.ndarray, np.ndarray]:
             building_counts = building_fractional_count[
                 rows - window.row_off, cols - window.col_off
             ]
-
-            lats_lons_list.append(lats_lons)
-            building_counts_list.append(building_counts)
-    lats_lons = np.concatenate(lats_lons_list) if lats_lons_list else np.array([])
-    building_counts = (
-        np.concatenate(building_counts_list) if building_counts_list else np.array([])
-    )
-
-    return lats_lons, building_counts
+            if lats_lons is not None or lats_lons.size != 0:
+                result.append(add_hexagons(lats_lons, building_counts))
+    return gpd.GeoDataFrame(pd.concat(result, ignore_index=True))
 
 
 def add_hexagons(lats_lons, building_counts):
@@ -126,25 +122,6 @@ def add_hexagons(lats_lons, building_counts):
     return gdf
 
 
-def process_file(file_path: str) -> pd.DataFrame:
-    """
-    Processes an image file and adds hexagons based on the extracted data.
-    """
-    failed_files = get_failed_files()
-    if file_path in failed_files:
-        return pd.DataFrame()
-    try:
-        lats_lons, building_counts = process_image(file_path)
-        if lats_lons is None or lats_lons.size == 0:
-            log_failed_file(file_path)
-            return pd.DataFrame()
-        return add_hexagons(lats_lons, building_counts)
-    except Exception as e:
-        logging.error("Error processing %s: %s", file_path, str(e))
-        log_failed_file(file_path)
-        return pd.DataFrame()
-
-
 @save_parquet_decorator(
     "bronze", BUILDING_CONTRACTS_BRONZE["buildings_google"], save_db=True, save_pq=True
 )
@@ -152,33 +129,31 @@ def save_batch_results(batch_df, **kwargs):
     """
     Saves the batch results and logs metrics to MLflow.
     """
-    filename = kwargs.get("filename", "default_filename")
-    with mlflow.start_run(run_name=filename, nested=True):
-        mlflow.log_metric("num_rows", batch_df.shape[0])
-        mlflow.log_metric("building_count", batch_df["building_count"].sum())
-        return batch_df
+    return batch_df
 
 
-def process_and_save(file_path, batch_number, parent_run_id):
+def process_and_save(file_path, batch_number):
     """
     Processes a single file and saves the results.
     """
     try:
-        with mlflow.start_run(run_id=parent_run_id):
-            result = process_file(file_path)
-            if result.empty:
-                log_failed_file(file_path)
-                return 0
-            kwargs = {"filename": f"batch_{batch_number}_{os.path.basename(file_path)}"}
-            save_batch_results(result, **kwargs)
-            result_len = len(result)
-            del result
-            gc.collect()
-            return result_len
+
+        result = process_image(file_path)
+        if result.empty:
+            log_failed_file(file_path)
+            return 0, 0
+        result["year"] = int(file_path.split("_")[3])
+        kwargs = {"filename": f"batch_{batch_number}_{os.path.basename(file_path)}"}
+        save_batch_results(result, **kwargs)
+        result_len = len(result)
+        result_dompp = result["building_count"].sum()
+        del result
+        gc.collect()
+        return result_len, result_dompp
     except Exception as e:
         write_log(f"An error occurred processing {file_path}: {e}")
         log_failed_file(file_path)
-        return 0
+        return 0, 0
 
 
 def get_remaining_files(path: str) -> List[str]:
@@ -253,7 +228,6 @@ def categorize_files_by_size(
 def process_files_in_parallel(
     file_paths: List[str],
     n_workers: int,
-    parent_run: mlflow.entities.Run,
     memory_limit: str,
 ):
     """
@@ -261,7 +235,6 @@ def process_files_in_parallel(
     Args:
         file_paths (List[str]): List of file paths to be processed.
         n_workers (int): Number of worker processes to use.
-        parent_run (mlflow.entities.Run): The parent run object containing run information.
         memory_limit (str): Memory limit for each worker process.
     Returns:
         int: Total number of processed rows.
@@ -280,7 +253,6 @@ def process_files_in_parallel(
             lambda row: process_and_save(
                 row["file_path"],
                 row.name // (len(file_paths) // n_workers),
-                parent_run.info.run_id,
             ),
             axis=1,
             meta=("processed_rows", "int64"),
@@ -289,70 +261,47 @@ def process_files_in_parallel(
         progress(futures)
 
         results = futures.compute()
-        total_processed = results.sum()
-    return total_processed
+        results_len = results["processed_rows"]
+        results_dompp = results["building_count"]
+        total_processed = results_len.sum()
+        total_dompp = results_dompp.sum()
+    return total_processed, total_dompp
 
 
 def main():
     """
     Main function to process image files into hex format using Dask for parallelization.
     """
-    parallel = True
-    run_time = time.strftime("%Y-%m-%d %H:%M:%S")
-    state = "SP"
-    if parallel:
-        with mlflow.start_run(run_name=f"{state}_{run_time}") as parent_run:
-            path = os.path.join(
-                BUILDING_CONTRACTS_RAW["buildings_google"]["physicalPath"], state
-            )
-            files = get_remaining_files(path)
-            nano_files, xsm_files, small_files, medium_files, large_files, xl_files = (
-                categorize_files_by_size(files)
-            )
-            if len(nano_files) > 0:
-                write_log(f"Processing nano files: {len(nano_files)}")
-                n_workers = 20
-                _ = process_files_in_parallel(
-                    nano_files, n_workers, parent_run, memory_limit="5GB"
-                )
-            if len(xsm_files) > 0:
-                write_log(f"Processing extra small files: {len(xsm_files)}")
-                n_workers = 15
-                _ = process_files_in_parallel(
-                    xsm_files, n_workers, parent_run, memory_limit="10GB"
-                )
-            if len(small_files) > 0:
-                write_log(f"Processing small files: {len(small_files)}")
-                n_workers = 10
-                _ = process_files_in_parallel(
-                    small_files, n_workers, parent_run, memory_limit="11GB"
-                )
-            if len(medium_files) > 0:
-                write_log(f"Processing medium files: {len(medium_files)}")
-                n_workers = 7
-                _ = process_files_in_parallel(
-                    medium_files, n_workers, parent_run, memory_limit="25GB"
-                )
-            if len(large_files) > 0:
-                write_log(f"Processing large files: {len(large_files)}")
-                n_workers = 3
-                _ = process_files_in_parallel(
-                    large_files, n_workers, parent_run, memory_limit="55GB"
-                )
-            if len(xl_files) > 0:
-                write_log(f"Processing extra large files: {len(xl_files)}")
-                n_workers = 1
-                _ = process_files_in_parallel(
-                    xl_files, n_workers, parent_run, memory_limit="62GB"
-                )
-
-    else:
+    col_sum = 0
+    dompp_sum = 0
+    with mlflow.start_run(run_name=f"{STATE}_{YEAR}_{RUN_TIME}"):
         path = os.path.join(
-            BUILDING_CONTRACTS_RAW["buildings_google"]["physicalPath"], state
+            BUILDING_CONTRACTS_RAW["buildings_google"]["physicalPath"], STATE
         )
-        files = ["/mnt/files/google_buildings/2016/MG/00a5c_2016_06_30_RTk0wGE47-Q.tif"]
-        for i, file in tqdm(enumerate(files)):
-            process_and_save(file, i, None)
+        files = get_remaining_files(path)
+        nano_files, xsm_files, small_files, medium_files, large_files, xl_files = (
+            categorize_files_by_size(files)
+        )
+        for file_group, n_workers, memory_limit in [
+            (xl_files, 10, "10GB"),
+            (large_files, 20, "9GB"),
+            (medium_files, 30, "8GB"),
+            (small_files, 40, "7GB"),
+            (xsm_files, 50, "6GB"),
+            (nano_files, 60, "5GB"),
+        ]:
+            if file_group:
+                write_log(
+                    f"Processing {file_group} {len(file_group)} files with {n_workers} workers"
+                )
+                n_workers = min(n_workers, len(file_group))
+                col_sum_batch, dompp_sum_batch = process_files_in_parallel(
+                    file_group, n_workers, memory_limit
+                )
+                col_sum += col_sum_batch
+                dompp_sum += dompp_sum_batch
+            mlflow.log_metric("total_rows", col_sum)
+            mlflow.log_metric("total_dompp", dompp_sum)
     manager.update_status("finished_step_2")
 
 
