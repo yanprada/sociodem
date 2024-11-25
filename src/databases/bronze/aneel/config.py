2fed3f310a42bcc9419deab9e7041f1c6f144ba7
Yan Prada <yanpradamoro@gmail.com>
2024-11-25 08:55:04 -0300

    :wrench: config (aneel bronze): change last execution id

--- src/databases/bronze/aneel/config.py
+++ src/databases/bronze/aneel/config.py
@@ -14,7 +14,7 @@ import os
 from src.tools.data_contract.aneel_data_contract import get_aneel_contracts
 from config.run_mode import DEBUG
 
-EXECUTION_ID = "bronze-aneel-NtfyqPzbmQweZrk"
+EXECUTION_ID = "bronze-aneel-M8vwCrw780mDmZp"
 
 BASE_PARAMS = {
     "medallon": "bronze",
