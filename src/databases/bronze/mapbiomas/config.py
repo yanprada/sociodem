ef27a521c506ff986fa8fa91dae83f60cf005155
Yan Prada <yanpradamoro@gmail.com>
2025-01-15 09:25:52 -0300

    :wrench: config (mapbiomas bronze): change execution id

--- src/databases/bronze/mapbiomas/config.py
+++ src/databases/bronze/mapbiomas/config.py
@@ -14,7 +14,7 @@ import os
 from src.tools.data_contract.mapbiomas_data_contract import get_mapbiomas_contracts
 from config.run_mode import DEBUG
 
-EXECUTION_ID = "bronze-mapbiomas-G7jJVhlhymC1oWf"
+EXECUTION_ID = "bronze-mapbiomas-4PxthI8ePWsEChc"
 BASE_PARAMS = {
     "medallon": "bronze",
     "data_name": "mapbiomas",
