92a615f74b0a004e7e62880e99492a04820fde84
Yan Prada <yanpradamoro@gmail.com>
2025-02-01 18:09:28 -0300

    :recycle: refactor (mapbiomas bronze): remove table grouped by hex and add year as parameter

--- src/databases/bronze/mapbiomas/steps/c_create_grouped_by_hex_mapbiomas.py
+++ src/databases/bronze/mapbiomas/steps/c_create_grouped_by_hex_mapbiomas.py
@@ -7,6 +7,7 @@ and stores the result in the 'bronze' table.
 """
 
 import os
+from tqdm import tqdm
 
 from src.tools.databases.data_connection.connection import DBConnection
 from src.tools.utils.common import get_db_path
@@ -24,45 +25,39 @@ manager.update_status(execution_parameters, module_name)
 CONTRACTS_BRONZE = execution_parameters["data_contracts"]["mapbiomas_bronze"]
 
 
-def create_grouped_by_hex_mapbiomas(conn: DBConnection) -> None:
+def create_indexes(year: int) -> None:
+    """
+    Creates indexes on the 'mapbiomas' table.
+
+    Args:
+        year (int): The year to be processed.
+    """
+    conn = DBConnection("bronze")
+    schema = CONTRACTS_BRONZE["mapbiomas"]["schema"]
+    table_name = CONTRACTS_BRONZE["mapbiomas"]["tableName"].format(year=year)
+    conn.create_index(schema, table_name, ["hex_col", "value"])
+    conn.close()
+
+
+def create_grouped_by_hex_mapbiomas(year: int) -> None:
     """
     Creates a new table with grouped data based on hex_col and value columns from an existing table.
 
     Args:
-        conn (DBConnection): The database connection object.
+        year (int): The year to be processed.
     """
+    conn = DBConnection("bronze")
     contract_mapbiomas = CONTRACTS_BRONZE["mapbiomas"]
     contract_mapbiomas_hex = CONTRACTS_BRONZE["grouped_by_hex_mapbiomas"]
-    old_path = get_db_path(contract_mapbiomas)
-    new_path = get_db_path(contract_mapbiomas_hex)
+    old_path = get_db_path(contract_mapbiomas).format(year=year)
+    new_path = get_db_path(contract_mapbiomas_hex).format(year=year)
     query = f"""
     SELECT hex_col, value, SUM(size) AS total_count
     FROM {old_path}
     GROUP BY hex_col, value;
     """
     conn.create_table_from_sql(query, new_path)
-
-
-def create_unique_hex_ids_mapbiomas(conn: DBConnection) -> None:
-    """
-    Create a table with unique hex IDs from the existing mapbiomas table.
-
-    Args:
-        conn (DBConnection): The database connection object.
-    """
-    contract_mapbiomas_hex = CONTRACTS_BRONZE["grouped_by_hex_mapbiomas"]
-    contract_mapbiomas_unique_hex = CONTRACTS_BRONZE["unique_hex_ids"]
-    old_path = get_db_path(contract_mapbiomas_hex)
-    new_path = get_db_path(contract_mapbiomas_unique_hex)
-    query = f"""
-    SELECT DISTINCT hex_col
-    FROM {old_path}
-    """
-
-    schema = contract_mapbiomas_unique_hex["schema"]
-    table = contract_mapbiomas_unique_hex["tableName"]
-    conn.create_table_from_sql(query, new_path)
-    conn.create_pk(schema, table, "col_id")
+    conn.close()
 
 
 def main() -> None:
@@ -73,9 +68,12 @@ def main() -> None:
     in the 'bronze' table,
     and stores the result in the 'bronze' table.
     """
-    conn = DBConnection("bronze")
-    create_grouped_by_hex_mapbiomas(conn)
-    create_unique_hex_ids_mapbiomas(conn)
+    year_init, year_end = CONTRACTS_BRONZE["mapbiomas"]["queryYears"]
+    for year in tqdm(
+        range(year_init, year_end), desc="Creating grouped by hex mapbiomas"
+    ):
+        create_indexes(year)
+        create_grouped_by_hex_mapbiomas(year)
     manager.update_last_run()
 
 
