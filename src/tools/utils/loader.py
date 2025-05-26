c3f553ceac7027cba2878e1ebb0e9bdff977279b
Yan Prada <yanpradamoro@gmail.com>
2025-05-26 12:46:40 -0300

    :recycle: refactor (loader): change get_places function

--- src/tools/utils/loader.py
+++ src/tools/utils/loader.py
@@ -16,11 +16,12 @@ import geopandas as gpd
 from src.tools.databases.data_connection.connection import DBConnection
 from src.tools.utils.constants import CRS_GLOBAL
 from src.tools.utils.common import get_db_path
-from src.tools.data_contract.pois_data_contract import get_pois_contracts
+
+# from src.tools.data_contract.pois_data_contract import get_pois_contracts
 from src.databases.bronze.ibge.config import CONTRACTS_BRONZE
 
 CONTRACT_CENSO_BRONZE = CONTRACTS_BRONZE
-CONTRACT_POIS_BRONZE = get_pois_contracts("bronze")
+# CONTRACT_POIS_BRONZE = get_pois_contracts("bronze")
 
 
 class Loader:
@@ -74,16 +75,16 @@ class Loader:
         query = f"SELECT DISTINCT cod_mun FROM {path}"
         return conn.query_database(query)["cod_mun"].tolist()
 
-    @staticmethod
-    def get_places():
-        """
-        Retrieves a list of distinct municipality codes from the specified database table.
+    # @staticmethod
+    # def get_places():
+    #     """
+    #     Retrieves a list of distinct municipality codes from the specified database table.
 
-        Returns:
-            list: A list of distinct municipality codes.
-        """
-        contract_dompp = CONTRACT_POIS_BRONZE["pois"]
-        path = get_db_path(contract_dompp)
-        conn = DBConnection("bronze")
-        query = f"SELECT cd_mun, nm_mun, general_category, hex_col FROM {path}"
-        return conn.query_database(query)
+    #     Returns:
+    #         list: A list of distinct municipality codes.
+    #     """
+    #     contract_dompp = CONTRACT_POIS_BRONZE["pois"]
+    #     path = get_db_path(contract_dompp)
+    #     conn = DBConnection("bronze")
+    #     query = f"SELECT cd_mun, nm_mun, general_category, hex_col FROM {path}"
+    #     return conn.query_database(query)
