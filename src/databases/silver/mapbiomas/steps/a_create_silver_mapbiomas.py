86fdb56c693eeec01f9e3ee10d5d7ecda4161538
Yan Prada <yanpradamoro@gmail.com>
2025-05-29 15:05:29 -0300

    :sparkles: feat (mapbiomas silver): change code to process yearly

--- src/databases/silver/mapbiomas/steps/a_create_silver_mapbiomas.py
+++ src/databases/silver/mapbiomas/steps/a_create_silver_mapbiomas.py
@@ -2,7 +2,7 @@
 This module contains functions for creating and processing MapBiomas data in the silver database.
 
 Functions:
-- save_mapbiomas_partition: Saves the MapBiomas partition DataFrame.
+- save_single_file: Saves the MapBiomas partition DataFrame.
 - save_mapbiomas: Saves the MapBiomas DataFrame.
 - calculate_percentage: Calculates the percentage of each value per hex_col.
 - pivot_table: Pivots the DataFrame.
@@ -12,18 +12,18 @@ Functions:
 """
 
 import os
-from functools import lru_cache
 import gc
 import multiprocessing
-import concurrent.futures
 from typing import List
+import dask
+from dask.distributed import Client, LocalCluster, as_completed
 import pandas as pd
 from tqdm import tqdm
 from src.tools.databases.data_connection.connection import DBConnection
 
 from src.tools.utils.constants import MAPBIOMAS_CLASSES
 from src.tools.managers.saver import save_parquet_decorator
-from src.tools.utils.common import write_log, get_db_path
+from src.tools.utils.common import write_log, get_db_path, trim_memory
 from src.databases.silver.mapbiomas.config import (
     manager,
     CONTRACTS_SILVER,
@@ -32,12 +32,13 @@ from src.databases.silver.mapbiomas.config import (
 )
 
 
-module_name = os.path.basename(__file__).replace(".py", "")
-manager.update_status(module_name)
+dask.config.set({"distributed.worker.memory.target": 0.8})
+dask.config.set({"distributed.worker.memory.spill": 0.9})
+dask.config.set({"distributed.worker.memory.pause": 0.95})
 
 
 @save_parquet_decorator("silver")
-def save_mapbiomas_partition(df: pd.DataFrame, **kwargs):
+def save_single_file(df: pd.DataFrame, **kwargs):
     """
     Saves the MapBiomas partition DataFrame.
 
@@ -54,28 +55,28 @@ def save_mapbiomas_partition(df: pd.DataFrame, **kwargs):
 
 def save_mapbiomas(
     df: pd.DataFrame,
-    large_partition: int,
+    year: int,
+    external_partition: int,
     partition: int,
-    batch: int,
-    external_batch: int,
+    minibatch: int,
 ):
     """
     Saves the MapBiomas DataFrame.
 
     Args:
         df (pd.DataFrame): The DataFrame containing the MapBiomas data.
-        large_partition (int): The large partition number.
+        year (int): The MapBiomas year being processed.
+        external_partition (int): The large partition number.
         partition (int): The partition number.
-        batch (int): The batch size.
-        external_batch  (int): The external batch size.
+        minibatch (int): The minibatch size.
     """
-    large_partition = int(large_partition / external_batch)
-    partition = int(partition / batch)
+    external_partition = int(external_partition / minibatch)
+    partition = int(partition / minibatch)
     kwargs = {
-        "filename": f"mapbiomas_{large_partition}_{partition}",
-        "contract": CONTRACTS_SILVER["grouped_by_hex_mapbiomas"],
+        "filename": f"mapbiomas_{external_partition}_{partition}",
+        "contract": CONTRACTS_SILVER[f"brasil_coverage_{year}"],
     }
-    _ = save_mapbiomas_partition(df, **kwargs)
+    _ = save_single_file(df, **kwargs)
 
 
 def calculate_percentage(df: pd.DataFrame):
@@ -91,7 +92,7 @@ def calculate_percentage(df: pd.DataFrame):
     """
     total_counts_per_hex_col = df.groupby("hex_col")["total_count"].transform("sum")
     df["pct"] = round((df["total_count"] / total_counts_per_hex_col) * 100, 2).astype(
-        "float16"
+        "float64"
     )
     return df.drop(columns="total_count")
 
@@ -109,6 +110,7 @@ def add_missing_columns(df: pd.DataFrame) -> pd.DataFrame:
     for col in MAPBIOMAS_CLASSES.values():
         if col not in df.columns:
             df[col] = 0
+        df[col] = df[col].astype("float64")
     return df
 
 
@@ -122,27 +124,31 @@ def pivot_mapbiomas_table(df: pd.DataFrame):
     Returns:
         pandas.DataFrame: The pivoted DataFrame.
     """
+
     df = df.pivot_table(
         index="hex_col", columns="value", values="pct", aggfunc="sum"
     ).fillna(0)
-    return add_missing_columns(df).reset_index()
+    df = add_missing_columns(df)
+    return df.reset_index()
 
 
-def load_mapbiomas(conn: DBConnection, condition: str) -> pd.DataFrame:
+def load_mapbiomas(condition: str, year: int) -> pd.DataFrame:
     """
     Load data from the bronze database.
 
     Args:
-        conn (DBConnection): The database connection object.
         condition (str): The condition to apply to the database query.
+        year(int): Mapbiomas year.
 
     Returns:
         pd.DataFrame: The loaded data from the bronze database.
     """
-    contract_mapbiomas = CONTRACTS_BRONZE["grouped_by_hex_mapbiomas"]
-    path = get_db_path(contract_mapbiomas)
-    query = f"SELECT * FROM {path} {condition}"
-    return conn.query_database(query)
+    with DBConnection("bronze") as conn:
+        contract_mapbiomas = CONTRACTS_BRONZE[f"grouped_by_hex_brasil_coverage_{year}"]
+        path = get_db_path(contract_mapbiomas)
+        query = f"SELECT * FROM {path} {condition}"
+        df = conn.query_database(query)
+    return df
 
 
 def add_classes_mapbiomas(df: pd.DataFrame):
@@ -159,113 +165,188 @@ def add_classes_mapbiomas(df: pd.DataFrame):
     return df
 
 
-def get_hex_ids(batch: int, i: int) -> List[str]:
-    """
-    Get all hex ids from the bronze database.
-
-    Args:
-        batch (int): The number of hex ids to retrieve in each batch.
-        i (int): The offset value to start retrieving hex ids.
-
-    Returns:
-        List[str]: The list of all hex ids.
-    """
-    conn = DBConnection("bronze")
-    contract_unique_hex = CONTRACTS_BRONZE["unique_hex_ids"]
-    path = get_db_path(contract_unique_hex)
-    range_col_ids = list(range(i, i + batch))
-    query_batch = f"SELECT hex_col FROM {path} WHERE col_id in {tuple(range_col_ids)}"
-    hex_list = conn.query_database(query_batch)["hex_col"].tolist()
-    conn.close()
-    return hex_list
-
-
-@lru_cache()
-def get_hex_len(year: int) -> int:
-    """
-    Retrieves the length of distinct hex values from the specified database connection.
-    Args:
-        year (int): The MapBiomas year being processed.
-
-    Returns:
-        The length of distinct hex values.
-
-    """
-    conn = DBConnection("bronze")
-    contract_unique_hex = CONTRACTS_BRONZE[f"grouped_by_hex_brasil_coverage_{year}"]
-    path = get_db_path(contract_unique_hex)
-    hex_num = int(
-        conn.query_database(f"SELECT COUNT(*) FROM {path}")["count"].values[0]
-    )
-    conn.close()
-    return hex_num
-
-
 def process_batch(
-    hex_ids: list, i: int, batch: int, external_batch: int, external_partition: int
+    hex_ids: list,
+    year: int,
+    i: int,
+    minibatch: int,
+    external_partition: int,
 ):
     """
     Process a batch of hex IDs to create the silver mapbiomas.
 
     Args:
         hex_ids (list): List of hex IDs to process.
+        year (int): The MapBiomas year being processed.
         i (int): Starting index of the batch.
-        batch (int): Number of hex IDs to process in each batch.
-        external_batch (int): External batch to save the mapbiomas.
+        minibatch (int): Number of hex IDs to process in each minibatch.
         external_partition (int): External partition to save the mapbiomas.
     """
-    conn = DBConnection("bronze")
-    query_hex_ids = hex_ids[i : i + batch]
-    condition = f"WHERE hex_col IN {tuple(query_hex_ids)}"
-    df = load_mapbiomas(conn, condition)
-    df = add_classes_mapbiomas(df)
-    df = calculate_percentage(df)
-    df = pivot_mapbiomas_table(df)
-    save_mapbiomas(df, external_partition, i, batch, external_batch)
-    del df
-    gc.collect()
-    conn.close()
+    try:
+        query_hex_ids = hex_ids[i : i + minibatch]
+        contract = CONTRACTS_SILVER[f"brasil_coverage_{year}"]
+        path_already_processed = get_db_path(contract)
+        with DBConnection("silver") as conn:
+            df_already_processed = conn.query_database(
+                f"""SELECT * FROM {path_already_processed} WHERE hex_col = '{query_hex_ids[0]}' """
+            )
+        if df_already_processed.empty:
+
+            condition = f"WHERE hex_col IN {tuple(query_hex_ids)}"
+            df = (
+                load_mapbiomas(condition, year)
+                .pipe(add_classes_mapbiomas)
+                .pipe(calculate_percentage)
+                .pipe(pivot_mapbiomas_table)
+            )
+
+            save_mapbiomas(df, year, external_partition, i, minibatch)
+            del df
+            gc.collect()
+        else:
+            write_log(
+                f"Skipping processing for {i} in year {year} "
+                f"as it has already been processed."
+            )
+    except Exception as e:
+        write_log(
+            f"Error processing batch {i} in year {year}: {e}",
+            "error",
+        )
+        raise e
+
+
+def get_hex_ids(year: int, external_partition: int, batch: int) -> List[str]:
+    """
+    Retrieves the length of distinct hex values from the specified database connection.
+    Args:
+        year (int): The MapBiomas year being processed.
+        external_partition (int): The external partition number.
+        batch (int): The batch size.
+    Returns:
+        List[str]: A list of distinct hex values for the specified year.
+
+    """
+    hex_col_list = []
+
+    row_numbers = range(external_partition, external_partition + batch)
+    with DBConnection("bronze") as conn:
+        contract_unique_hex = CONTRACTS_BRONZE[f"unique_hex_{year}"]
+        write_log("Querying distinct hex_col values")
+        path = get_db_path(contract_unique_hex)
+        df = conn.query_database(
+            f"""SELECT hex_col FROM {path} WHERE row_number IN {tuple(row_numbers)}"""
+        )
+        hex_col_list = df["hex_col"].to_list()
+        del df
+        gc.collect()
+    write_log("Finished retrieving hex_col values")
+    return hex_col_list
+
+
+def create_hex_unique_ids_table(year: int) -> None:
+    """
+    Creates a table with distinct hex_col values for the specified year.
+    Args:
+        year (int): The MapBiomas year being processed.
+    """
+    with DBConnection("bronze") as conn:
+        contract_hex = CONTRACTS_BRONZE[f"grouped_by_hex_brasil_coverage_{year}"]
+        contract_unique_hex = CONTRACTS_BRONZE[f"unique_hex_{year}"]
+        new_path = get_db_path(contract_unique_hex)
+        write_log("Creating distinct hex_col table")
+        path = get_db_path(contract_hex)
+        query = f"""
+                SELECT 
+                    ROW_NUMBER() OVER () AS row_number,
+                    hex_col
+                FROM (
+                    SELECT DISTINCT hex_col
+                    FROM {path}
+                    ) AS distinct_hexes;
+            """
+        conn.create_table_from_sql(query, new_path)
+        conn.create_index(
+            contract_unique_hex["schema"],
+            contract_unique_hex["tableName"],
+            ["row_number"],
+        )
+
+
+def get_hex_counts(year: int) -> int:
+    """
+    Retrieves the number of distinct hex values for the specified year.
+
+    Args:
+        year (int): The MapBiomas year being processed.
+
+    Returns:
+        int: The number of distinct hex values for the specified year.
+    """
+    with DBConnection("bronze") as conn:
+        contract_unique_hex = CONTRACTS_BRONZE[f"grouped_by_hex_brasil_coverage_{year}"]
+        schema = contract_unique_hex["schema"]
+        table_name = contract_unique_hex["tableName"]
+        write_log("Creating index for hex_col")
+        conn.create_index(schema, table_name, ["hex_col"])
+        write_log("Querying sum distinct hex_col values")
+        path = get_db_path(contract_unique_hex)
+        hex_count = int(
+            conn.query_database(
+                f"""SELECT COUNT(DISTINCT(hex_col)) as count FROM {path}"""
+            )["count"].values[0]
+        )
+    return hex_count
 
 
 def main():
     """
     This function retrieves data from the "bronze" database, processes it, and returns a DataFrame.
     """
+
+    module_name = os.path.basename(__file__).replace(".py", "")
+    manager.update_status(module_name)
+
     batch = int(5e6)
-    external_batch = int(5e6)
+    minibatch = int(batch / 10)
     for year in tqdm(YEARS, desc="Processing Years"):
-        hex_len = get_hex_len(year)
+        hex_len = get_hex_counts(year)
+        create_hex_unique_ids_table(year)
+        # Ensure that there is the table and schema in the database
+        # befor processing the data in parallel
+        hex_ids = get_hex_ids(year, 0, minibatch)
+        process_batch(hex_ids, year, 0, minibatch, 0)
         for external_partition in tqdm(
-            range(0, hex_len, external_batch),
-            desc="Processing data in external batch",
+            range(minibatch, hex_len, batch),
+            desc="Processing data in batch",
         ):
-            hex_ids = get_hex_ids(external_batch, external_partition)
-            num_cores = min(1, multiprocessing.cpu_count())
-            with concurrent.futures.ProcessPoolExecutor(
-                max_workers=num_cores
-            ) as executor:
+            hex_ids = get_hex_ids(year, external_partition, batch)
+            num_cores = min(6, multiprocessing.cpu_count())
+            cluster = LocalCluster(
+                n_workers=num_cores, threads_per_worker=1, processes=True
+            )
+            with Client(cluster) as client:
                 futures = [
-                    executor.submit(
+                    client.submit(
                         process_batch,
                         hex_ids,
+                        year,
                         i,
-                        batch,
-                        external_batch,
+                        minibatch,
                         external_partition,
                     )
-                    for i in range(0, len(hex_ids), batch)
+                    for i in range(minibatch, len(hex_ids), minibatch)
                 ]
 
                 for future in tqdm(
-                    concurrent.futures.as_completed(futures),
+                    as_completed(futures),  # type: ignore
                     total=len(futures),
-                    desc="Processing data in batch",
+                    desc="Processing data in minibatch",
                 ):
                     try:
-                        future.result()
+                        future.result()  # type: ignore
+                        trim_memory()
 
                     except Exception as e:
                         write_log(e, "error")
-            del hex_ids
-            gc.collect()
-    manager.update_last_run()
+            cluster.close()
