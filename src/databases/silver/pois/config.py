7b38b90cfaafaf7a5c230eebdeb3be80bf1bcb36
Yan Prada <yanpradamoro@gmail.com>
2025-06-17 19:39:51 -0300

    :recycle: refactor (pois silver): change script for new project structure

--- src/databases/silver/pois/config.py
+++ src/databases/silver/pois/config.py
@@ -12,16 +12,32 @@ Attributes:
 
 import os
 
-from src.tools.data_contract.pois_data_contract import get_pois_contracts
+from src.tools.managers.execution import ExecutionManagerWrapper
 
 
-EXECUTION_ID = "silver-pois-EuKrqaMAD1JmO5c"
+EXECUTION_ID = "silver-pois-2025-06-17-16h47m19s"
 
 BASE_PARAMS = {
     "medallon": "silver",
     "data_name": "pois",
     "config_path": os.path.abspath(__file__),
-    "data_contracts": [get_pois_contracts("bronze"), get_pois_contracts("silver")],
+    "data_contracts": {
+        "pois_bronze": ["bronze", "pois", "places"],
+        "pois_silver": ["silver", "pois", "places"],
+        "silver_data_2022": ["silver", "ibge", "ibge_2022"],
+    },
     "run_mode": "dev",
     "last_run": None,
+    "materialized_views": {},
 }
+
+
+_manager_wrapper = ExecutionManagerWrapper(BASE_PARAMS, EXECUTION_ID, overwrite=True)
+manager = _manager_wrapper.manager
+
+CONTRACTS_BRONZE = manager.execution_details["data_contracts"]["pois_bronze"]
+CONTRACTS_SILVER = manager.execution_details["data_contracts"]["pois_silver"]
+CONTRACTS_CENSO_SILVER = manager.execution_details["data_contracts"]["silver_data_2022"]
+YEARS = manager.execution_details["info"]["running_years"]
+
+EXPERIMENT_NAME = manager.execution_details["mlflow_experiment"]
--- src/databases/silver/pois/steps/a_create_places_hex.py
+++ src/databases/silver/pois/steps/a_create_places_hex.py
@@ -2,32 +2,37 @@
 This module contains functions for creating and processing Places data in the silver database.
 """
 
+import os
 from typing import List
+import geopandas as gpd
 import pandas as pd
 
 from src.tools.managers.loader import Loader
 from src.tools.managers.saver import save_parquet_decorator
-from src.tools.managers.execution import ExecutionManager
-from src.databases.silver.pois.config import EXECUTION_ID, BASE_PARAMS
+from src.tools.databases.data_connection.connection import DBConnection
+from src.tools.utils.common import get_db_path
+from src.tools.utils.constants import CRS_GLOBAL
+from src.tools.utils.h3 import get_h3_geom
+
+from src.databases.silver.pois.config import (
+    CONTRACTS_SILVER,
+    CONTRACTS_CENSO_SILVER,
+    manager,
+)
+
+module_name = os.path.basename(__file__).replace(".py", "")
+manager.update_status(module_name)
 
 
-manager = ExecutionManager(BASE_PARAMS)
-execution_parameters = manager.get_execution_details(EXECUTION_ID, overwrite=True)
-manager.update_status("running_step_1")
-
-CONTRACT_BRONZE = execution_parameters["data_contracts"][0]
-CONTRACT_SILVER = execution_parameters["data_contracts"][1]
-
-
-def group_pois_per_hex(df_pois: pd.DataFrame, cols: List[str]) -> pd.DataFrame:
+def group_pois_per_cols(df_pois: pd.DataFrame, cols: List[str]) -> pd.DataFrame:
     """
-    Groups the pois data per hexagon.
+    Groups the pois data per cols.
 
     Args:
         df_pois (pd.DataFrame): The pois data.
         cols (List[str]): The columns to group by.
     Returns:
-        pd.DataFrame: The pois data grouped per hexagon.
+        pd.DataFrame: The pois data grouped per cols.
     """
     return (
         df_pois.groupby(cols + ["general_category"])
@@ -69,24 +74,73 @@ def calculate_stats(df_pois: pd.DataFrame) -> pd.DataFrame:
     return df_pois
 
 
-@save_parquet_decorator("silver", CONTRACT_SILVER["pois_hex"])
-def transform_pois(df_pois: pd.DataFrame) -> pd.DataFrame:
+def get_hex_mun_map(hex_ids: List[str]) -> pd.DataFrame:
+    """
+    Queries the municipalities data for the given hexagon IDs.
+
+    Args:
+        hex_ids (List[str]): The list of hexagon IDs.
+
+    Returns:
+        pd.DataFrame: The municipalities data.
+    """
+    path = get_db_path(CONTRACTS_CENSO_SILVER["hex_unique_sc_2022_sc_info"])
+    query = f"""
+    SELECT hex_col, cd_mun, nm_mun FROM {path}
+    WHERE hex_col IN ({', '.join(f"'{hex_id}'" for hex_id in hex_ids)})
+    """
+    with DBConnection("silver") as conn:
+        df_hex_mun = conn.query_database(query)
+    return df_hex_mun.set_index("hex_col")
+
+
+def calculate_pct_pois_per_hex(df_pois_mun, df_pois_hex):
+    """
+    Calculates the percentage of points of interest (POIs) per hexagon.
+    Args:
+        df_pois_mun (pd.DataFrame): DataFrame containing POIs aggregated by municipality.
+        df_pois_hex (pd.DataFrame): DataFrame containing POIs aggregated by hexagon.
+    Returns:
+        pd.DataFrame: DataFrame with the percentage of POIs per hexagon.
+    """
+    df_pois_mun = df_pois_mun.reset_index()
+    df_pois_hex = df_pois_hex.reset_index()
+    cols_to_normalize = [
+        col for col in df_pois_hex.columns if col not in ["cd_mun", "nm_mun", "hex_col"]
+    ]
+    df_merged = df_pois_hex.merge(
+        df_pois_mun, on=["cd_mun", "nm_mun"], suffixes=("_hex", "_mun")
+    )
+    for col in cols_to_normalize:
+        df_merged[col + "_pct"] = df_merged[col + "_hex"] / df_merged[col + "_mun"]
+    df_merged["geometry"] = df_merged["hex_col"].apply(get_h3_geom)
+    df_merged = gpd.GeoDataFrame(df_merged, geometry="geometry", crs=CRS_GLOBAL)
+    return df_merged
+
+
+@save_parquet_decorator("silver")
+def transform_pois(df_pois: pd.DataFrame, **kwargs) -> pd.DataFrame:
     """
     Transforms the pois data.
 
     Args:
         df_pois (pd.DataFrame): The pois data.
+        **kwargs: Additional arguments, including filename and contract.
 
     Returns:
         pd.DataFrame: The transformed pois data.
     """
-    cols_hex = ["cd_mun", "nm_mun", "hex_col"]
+    cols_hex = ["hex_col"]
     cols_mun = ["cd_mun", "nm_mun"]
-    df_pois_hex = group_pois_per_hex(df_pois, cols_hex).pipe(pivot_pois, cols_hex)
-    df_pois_mun = group_pois_per_hex(df_pois, cols_mun).pipe(pivot_pois, cols_mun)
-    df_pois = df_pois_hex.div(df_pois_mun, axis=0).fillna(0)
-    # revisar: check if it is correct
-    return df_pois
+    df_pois_hex = group_pois_per_cols(df_pois, cols_hex).pipe(pivot_pois, cols_hex)
+    hex_ids = df_pois_hex.index.tolist()
+    df_hex_mun = get_hex_mun_map(hex_ids)
+    df_pois_hex = (
+        df_pois_hex.join(df_hex_mun, how="inner").reset_index().set_index(cols_mun)
+    )
+    df_pois = df_pois.set_index("hex_col").join(df_hex_mun, how="inner")
+    df_pois_mun = group_pois_per_cols(df_pois, cols_mun).pipe(pivot_pois, cols_mun)
+    return calculate_pct_pois_per_hex(df_pois_mun, df_pois_hex)
 
 
 def main():
@@ -98,6 +152,5 @@ def main():
 
     loader = Loader()
     df_pois = loader.get_places()
-    _ = transform_pois(df_pois)
-    manager.update_status("finished_step_1")
-    manager.update_last_run()
+    kwargs = {"filename": "pois", "contract": CONTRACTS_SILVER["pois"]}
+    _ = transform_pois(df_pois, **kwargs)
